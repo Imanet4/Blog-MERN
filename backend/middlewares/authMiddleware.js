6a79587c29c6cfd22573e09a2e6de515d26f72f0
Imanet4 <imanetaifour2@gmail.com>
2025-07-18 12:52:19 +0100

    updates-post-backend-testing

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -6,12 +6,17 @@ const authMiddleware = async (req, res, next) => {
     let token;
     if(
         req.headers.authorization &&
-        res.headers.authorization.startsWith('Bearer')
+        req.headers.authorization.startsWith('Bearer')
     ){
         try {
             token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            console.log('Decoded Token:', decoded);
+
             req.user = await User.findById(decoded.id).select('-password');
+            console.log('req.user:', req.user);
+
+            
             next();
         }catch (error) {
             console.error(error);
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { register, login, logout } = require('../controllers/authController');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
+const User = require('../models/user');
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
 router.get('/me', authMiddleware, async (req, res) =>{
     try {
-        const user = await user.findById(req.user.id)
+        const user = await User.findById(req.user.id)
         res.status(200).json({
             id: user._id,
             username: user.username,
